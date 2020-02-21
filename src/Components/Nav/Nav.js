95fc950dd7d5bf2aaee2ae317283e6e578c9ac99
Preston Jenkins <prestonjenkins@Prestons-MacBook-Pro.local>
2020-02-21 12:35:25 -0700

    Login button not working from backend.

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,23 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { connect } from 'react-redux'
 
 
 class Nav extends React.Component {
   constructor(props) {
     super(props);
 
-    // this.state = {
-
-    // }
+    this.state = {
+      username: '',
+      imageURL: '',
+    }
   }
-
-
 
   render() {
     return (
       <div className="nav-body">
-        <div>
-          User image and name
+        <div className='profile-tag'>
+          <img src={this.state.imageURL} />
+          <p>{this.state.username}</p>
         </div>
         <div className="nav-buttons">
           <Link to="/dashboard">Home</Link>
@@ -33,4 +34,12 @@ class Nav extends React.Component {
   }
 }
 
-export default Nav;
+const mapStateToProps = reduxState => {
+  const { username, imageURL } = reduxState
+  return {
+    username,
+    imageURL,
+  }
+}
+
+export default connect(mapStateToProps)(Nav);
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,34 +1,36 @@
-// const initialState = {
-//   user: {}
-// }
+const initialState = {
+  user_id: 0,
+  username: '',
+  imageURL: '',
+}
 
-// const GET_USER = 'GET_USER';
-// const LOGOUT = 'LOGOUT';
+const GET_USER = 'GET_USER';
+const LOGOUT = 'LOGOUT';
 
-// export function getUser(userObj){
-//   return {
-//       type: GET_USER,
-//       payload: userObj,
-//   }
-// }
+export function getUser(userObj){
+  return {
+      type: GET_USER,
+      payload: userObj,
+  }
+}
 
-// export function logout(){
-//   return {
-//       type: LOGOUT,
-//       payload: null,
-//   }
-// }
+export function logout(){
+  return {
+      type: LOGOUT,
+      payload: null,
+  }
+}
 
-// export default function reducer(state = initialState, action){
-//   const {type, payload} = action;
-//   switch(type){
-//       case GET_USER:
-//           return {...state, user: payload}
-//           //{user: {}, user: {user_id: 1, user_email: 'email'}}
-//       case LOGOUT:
-//           return {...state, user: {}}
-//           //return initialState
-//       default:
-//           return state;
-//   }
-// };
+export default function reducer(state = initialState, action){
+  const {type, payload} = action;
+  switch(type){
+      case GET_USER:
+          return {...state, user: payload}
+          //{user: {}, user: {user_id: 1, user_email: 'email'}}
+      case LOGOUT:
+          return {...state, user: {}}
+          //return initialState
+      default:
+          return state;
+  }
+};
